fe73220c7131b91ced644d93762c4320852ac0e9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-08-29 06:52:11 +0300

    Auto-update headers via pre_commit.ps1 - 2025-08-29

--- FLCompanionByDvurechensky/Models/Graph.cs
+++ FLCompanionByDvurechensky/Models/Graph.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 августа 2025 07:08:08
- * Version: 1.0.30
+ * Last Updated: 29 августа 2025 06:52:09
+ * Version: 1.0.31
  */
 
 using System.Collections.Generic;
