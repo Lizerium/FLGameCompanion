856f9aa638f9d049f1606ddf058c185dca6ff4a6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-08 06:52:10 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-08

--- FLCompanionByDvurechensky/Models/Dijkstra.cs
+++ FLCompanionByDvurechensky/Models/Dijkstra.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 ноября 2025 06:52:05
- * Version: 1.0.101
+ * Last Updated: 08 ноября 2025 06:52:07
+ * Version: 1.0.102
  */
 
 using FLCompanionByDvurechensky.Data;
