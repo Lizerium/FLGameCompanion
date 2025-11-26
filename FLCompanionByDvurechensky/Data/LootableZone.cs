d2c1a1b8990fc321502ff569442bf5cc9977c231
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-26 06:52:05 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-26

--- FLCompanionByDvurechensky/Data/LootableZone.cs
+++ FLCompanionByDvurechensky/Data/LootableZone.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 ноября 2025 06:52:00
- * Version: 1.0.119
+ * Last Updated: 26 ноября 2025 06:52:03
+ * Version: 1.0.120
  */
 
 namespace FLCompanionByDvurechensky.Data
