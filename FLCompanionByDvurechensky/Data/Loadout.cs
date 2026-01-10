fd04fa524f352d84d29211b277c5d5d64df9698c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-10 06:52:05 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-10

--- FLCompanionByDvurechensky/Data/Loadout.cs
+++ FLCompanionByDvurechensky/Data/Loadout.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 января 2026 16:11:08
- * Version: 1.0.164
+ * Last Updated: 10 января 2026 06:52:03
+ * Version: 1.0.165
  */
 
 using System.Collections.Generic;
