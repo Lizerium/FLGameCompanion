1ed842b0ec38a175112507d8fe7ac96859e69734
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-09 06:52:11 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-09

--- FLCompanionByDvurechensky/Models/DataEdge.cs
+++ FLCompanionByDvurechensky/Models/DataEdge.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 февраля 2026 07:55:24
- * Version: 1.0.194
+ * Last Updated: 09 февраля 2026 06:52:09
+ * Version: 1.0.195
  */
 
 using GraphX.Common.Models;
--- FLCompanionByDvurechensky/Properties/Settings.Designer.cs
+++ FLCompanionByDvurechensky/Properties/Settings.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 февраля 2026 07:55:24
- * Version: 1.0.194
+ * Last Updated: 09 февраля 2026 06:52:09
+ * Version: 1.0.195
  */
 
 //------------------------------------------------------------------------------
@@ -20,7 +20,7 @@ namespace FLCompanionByDvurechensky.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
