b7f1c18422f8c172e25568209ba9c9c70b8c4155
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-31 10:53:19 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-31

--- FLCompanionByDvurechensky/Data/Equipment.cs
+++ FLCompanionByDvurechensky/Data/Equipment.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 марта 2026 12:20:48
- * Version: 1.0.244
+ * Last Updated: 31 марта 2026 10:53:17
+ * Version: 1.0.245
  */
 
 namespace FLCompanionByDvurechensky.Data
