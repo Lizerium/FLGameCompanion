88a75f87a6c1d9cbf3a90378a8498d23bf5d7f1f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-02 06:52:10 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-02

--- FLCompanionByDvurechensky/Data/ComboBoxItem.cs
+++ FLCompanionByDvurechensky/Data/ComboBoxItem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 апреля 2026 13:05:44
- * Version: 1.0.246
+ * Last Updated: 02 апреля 2026 06:52:07
+ * Version: 1.0.248
  */
 
 namespace FLCompanionByDvurechensky.Data
