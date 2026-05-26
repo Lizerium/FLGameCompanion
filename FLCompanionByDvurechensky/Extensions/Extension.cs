e8d6d076dfd9c3aaa9fdd2254d04ad3df3349f8d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-26 11:42:42 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-26

--- FLCompanionByDvurechensky/Extensions/Extension.cs
+++ FLCompanionByDvurechensky/Extensions/Extension.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 мая 2026 11:12:17
- * Version: 1.0.303
+ * Last Updated: 26 мая 2026 11:42:39
+ * Version: 1.0.304
  */
 
 namespace Freelancer_Companion_by_Dormammu.Extensions
