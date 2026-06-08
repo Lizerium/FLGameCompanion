e4b8e696889cd8926fcb5a7054d220ba04841c1f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-08 06:51:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-08

--- FLCompanionByDvurechensky/Models/GraphEdge.cs
+++ FLCompanionByDvurechensky/Models/GraphEdge.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июня 2026 18:21:09
- * Version: 1.0.316
+ * Last Updated: 08 июня 2026 06:51:50
+ * Version: 1.0.317
  */
 
 /// <summary>
