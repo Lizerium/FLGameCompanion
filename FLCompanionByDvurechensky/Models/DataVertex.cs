1b9466ca72fc098438f52c45bf9f2951fca76a32
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-12 06:52:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-12

--- FLCompanionByDvurechensky/Models/DataVertex.cs
+++ FLCompanionByDvurechensky/Models/DataVertex.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 июня 2026 13:26:06
- * Version: 1.0.320
+ * Last Updated: 12 июня 2026 06:52:04
+ * Version: 1.0.321
  */
 
 using GraphX;
