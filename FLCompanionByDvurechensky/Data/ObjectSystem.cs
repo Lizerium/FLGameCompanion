fa652a03120005e6af16cbff0d978d24a24ad68b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-25 16:30:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-25

--- FLCompanionByDvurechensky/Data/ObjectSystem.cs
+++ FLCompanionByDvurechensky/Data/ObjectSystem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 июня 2026 10:34:10
- * Version: 1.0.333
+ * Last Updated: 25 июня 2026 16:30:11
+ * Version: 1.0.334
  */
 
 namespace FLCompanionByDvurechensky.Data
