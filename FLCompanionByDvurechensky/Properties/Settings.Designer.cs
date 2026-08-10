68474972ad2d96eb0d8b69ebb60ffff976ae262b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-10 06:52:08 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-10

--- FLCompanionByDvurechensky/Properties/Settings.Designer.cs
+++ FLCompanionByDvurechensky/Properties/Settings.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 августа 2026 15:30:47
- * Version: 1.0.380
+ * Last Updated: 10 августа 2026 06:52:06
+ * Version: 1.0.381
  */
 
 //------------------------------------------------------------------------------
